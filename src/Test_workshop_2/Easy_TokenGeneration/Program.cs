7ad88759aeb5c6d0fb948613f489446f35a46c5b
Alexander <sany199574@gmail.com>
2024-10-11 00:06:33 +0300

    Убран лишний пробел

--- src/Test_workshop_2/Easy_TokenGeneration/Program.cs
+++ src/Test_workshop_2/Easy_TokenGeneration/Program.cs
@@ -1,5 +1,4 @@
-﻿
-// Секретный ключ для подписи JWT
+﻿// Секретный ключ для подписи JWT
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
