dbea8f480978f8df4c95584e39ca3c97b77bbcc0
Alexander <sany199574@gmail.com>
2024-10-13 01:49:32 +0300

    Добавлена программа  - Cache_Average_MemoryCache

---
+++ src/Test_workshop_2/Cache_Average_MemoryCache/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Cache_Average_MemoryCache.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ src/Test_workshop_2/Cache_Average_MemoryCache/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Cache_Average_MemoryCache.Models;
+using Cache_Average_MemoryCache.Services;
+using System.Runtime.Caching;
+
+MemoryCache cache = MemoryCache.Default;
+string cacheKey = "user_123";
+User user;
+UserService userService = new UserService(); // Экземпляр сервиса
+
+if (cache.Contains(cacheKey))
+{
+    user = cache.Get(cacheKey) as User;
+    Console.WriteLine($"Пользователь получен из кэша: {user.Name}, {user.Email}");
+}
+else
+{
+    // Используем UserService для получения данных пользователя
+    user = userService.GetUserById(123);
+
+    CacheItemPolicy policy = new CacheItemPolicy
+    {
+        AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(1)
+    };
+    cache.Add(cacheKey, user, policy);
+
+    Console.WriteLine($"Пользователь загружен из базы данных: {user.Name}, {user.Email}");
+}
+
+
+Console.ReadLine();
