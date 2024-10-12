26570bad46f15eddaf98e87de94fa59079d0fb29
Alexander <sany199574@gmail.com>
2024-10-13 02:05:11 +0300

    Добавлен проект. имитирующий работу Redis

---
+++ src/Test_workshop_2/Cache_Redis/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Cache_Redis.Models;
+using Cache_Redis.Services;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+
+
+string cacheKey = "user_123"; // Ключ для хранения данных конкретного пользователя
+User user;
+UserService userService = new UserService(); // Экземпляр сервиса
+
+
+// Получаем доступ к базе данных Redis
+IDatabase db = redis.GetDatabase();
+
+// Пытаемся получить данные пользователя из кэша
+string cachedUser = db.StringGet(cacheKey);
+if (!string.IsNullOrEmpty(cachedUser))
+{
+    // Данные пользователя найдены в кэше
+    user = JsonConvert.DeserializeObject<User>(cachedUser);
+    Console.WriteLine($"Пользователь получен из Redis: {user.Name}, {user.Email}");
+}
+else
+{
+    // Данных в кэше нет, загружаем их из базы данных
+    user = userService.GetUserById(123);
+
+    // Сериализуем объект пользователя в строку и сохраняем его в Redis на 60 секунд
+    db.StringSet(cacheKey, JsonConvert.SerializeObject(user), TimeSpan.FromSeconds(60));
+
+    Console.WriteLine($"Пользователь загружен из базы данных и сохранён в Redis: {user.Name}, {user.Email}");
+}
+
+// Имитация повторного запроса
+Console.WriteLine("\nИмитируем повторный запрос...");
+Thread.Sleep(3000); // Подождем 3 секунды
+
+// Снова пытаемся получить пользователя из Redis
+cachedUser = db.StringGet(cacheKey);
+if (!string.IsNullOrEmpty(cachedUser))
+{
+    user = JsonConvert.DeserializeObject<User>(cachedUser);
+    Console.WriteLine($"Повторно получен пользователь из Redis: {user.Name}, {user.Email}");
+}
+else
+{
+    Console.WriteLine("Данные пользователя больше не находятся в Redis.");
+}
+
+Console.ReadLine();
---
+++ src/Test_workshop_2/Cache_Redis/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Cache_Redis.Models;
+
+namespace Cache_Redis.Services
+{
+    public class UserService
+    {
+        // Метод для получения пользователя из базы данных по ID
+        public User GetUserById(int userId)
+        {
+            Console.WriteLine("Имитируем запрос к базе данных...");
+            // В реальном приложении здесь был бы запрос к базе данных
+            Thread.Sleep(2000); // Имитация задержки на 2 секунды
+
+            return new User
+            {
+                Id = userId,
+                Name = "Иван Иванов",
+                Email = "ivan.ivanov@example.com"
+            };
+        }
+    }
+}
