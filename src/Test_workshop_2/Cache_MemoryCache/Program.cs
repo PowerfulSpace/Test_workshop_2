f9adfc24efe76d2bde4d59f40c0de9138fb19293
Alexander <sany199574@gmail.com>
2024-10-12 06:23:17 +0300

    Добавлено приложение, по хешированию

---
+++ src/Test_workshop_2/Cache_MemoryCache/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Caching;
+
+MemoryCache cache = MemoryCache.Default;
+
+string cacheKey = "myKey";
+string cachedData = cache[cacheKey] as string;
+
+if (cachedData == null)
+{
+    cachedData = "This is some data to cache!";
+    CacheItemPolicy policy = new CacheItemPolicy { AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(5) };
+    cache.Add(cacheKey, cachedData, policy);
+    Console.WriteLine("Data added to cache.");
+}
+else
+{
+    Console.WriteLine("Data retrieved from cache: " + cachedData);
+}
+
+
+Console.ReadLine();
