a5ae15f8978e0700eaf938727f8e1e4063b9e9b9
Alexander <sany199574@gmail.com>
2024-10-15 02:17:38 +0300

    Добавлен проект   - TestApp_RabbitMQ_Producer

---
+++ src/Test_workshop_2/TestApp_RabbitMQ_Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+// Устанавливаем соединение с сервером RabbitMQ
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    // Объявляем очередь, в которую будем отправлять сообщения
+    channel.QueueDeclare(queue: "hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    string message = "Hello RabbitMQ!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    // Отправляем сообщение в очередь
+    channel.BasicPublish(exchange: "",
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Sent {message}");
+}
+
+Console.ReadLine();
