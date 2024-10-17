44428bf96e692280662db72a966a471f69d8e892
Alexander <sany199574@gmail.com>
2024-10-17 12:58:29 +0300

    Добавлено тестовое приложение для работы в бд PostgreSQL

---
+++ src/Test_workshop_2/Test_App_Easy_PostgreSQL/Program.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+
+var connectionString = "Host=localhost;Username=postgres;Password=yourpassword;Database=mydatabase";
+using (var connection = new NpgsqlConnection(connectionString))
+{
+    connection.Open();
+    using (var cmd = new NpgsqlCommand("SELECT * FROM mytable", connection))
+    {
+        using (var reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0)); // Чтение первой колонки
+            }
+        }
+    }
+}
