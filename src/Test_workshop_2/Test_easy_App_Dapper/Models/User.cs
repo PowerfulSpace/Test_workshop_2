341b1fa395dec31a868e0c8d32347d5bbc657d55
Alexander <sany199574@gmail.com>
2024-10-19 12:14:36 +0300

    Добавлен тестовый проект

---
+++ src/Test_workshop_2/Test_easy_App_Dapper/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Test_easy_App_Dapper.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ src/Test_workshop_2/Test_easy_App_Dapper/Program.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+using Test_easy_App_Dapper.Models;
+
+string connectionString = "YourConnectionStringHere";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+
+    // Выполнение SQL-запроса и сопоставление результата с объектом C#
+    var users = connection.Query<User>("SELECT * FROM Users").ToList();
+
+    foreach (var user in users)
+    {
+        Console.WriteLine($"{user.Id}: {user.Name}");
+    }
+}
+
+Console.ReadLine();
