23466b6bb65fb0f6dd75adc6db8b917bf77ca43a
Alexander <sany199574@gmail.com>
2024-10-19 15:14:14 +0300

    Добавлен конструктор в приложениие

--- src/Test_workshop_2/Test_app_SQLiete/Data/AppDbContext.cs
+++ src/Test_workshop_2/Test_app_SQLiete/Data/AppDbContext.cs
@@ -7,6 +7,12 @@ namespace Test_app_SQLiete.Data
     {
         public DbSet<User> Users { get; set; }
 
+        // Конструктор, который принимает DbContextOptions
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=mydatabase.db");
