863e0b1ffbf058c0629c954583e95cc4373e04f9
Alexander <sany199574@gmail.com>
2024-10-19 15:13:48 +0300

    Исправлен проект

---
+++ src/Test_workshop_2/Test_app_SQLiete/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_app_SQLiete.Models;
+
+namespace Test_app_SQLiete.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=mydatabase.db");
+        }
+    }
+}
--- src/Test_workshop_2/Test_app_SQLiete/Program.cs
+++ src/Test_workshop_2/Test_app_SQLiete/Program.cs
@@ -1,32 +1,21 @@
 ﻿// Указание пути к базе данных (если файла не существует, он будет создан)
-string cs = "Data Source=mydatabase.db";
+using Test_app_SQLiete.Data;
+using Test_app_SQLiete.Models;
 
-// Создаем подключение к базе данных
-using var con = new SQLiteConnection(cs);
-con.Open();
 
-// Создаем команду для выполнения SQL-запросов
-using var cmd = new SQLiteCommand(con);
-
-// Создание таблицы (если таблица не существует)
-cmd.CommandText = @"CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)";
-cmd.ExecuteNonQuery();
-
-// Вставка данных
-cmd.CommandText = "INSERT INTO users(name, age) VALUES('Alice', 25)";
-cmd.ExecuteNonQuery();
-
-// Запрос данных из таблицы
-cmd.CommandText = "SELECT * FROM users";
-
-// Чтение данных
-using SQLiteDataReader rdr = cmd.ExecuteReader();
-
-while (rdr.Read())
+using (var db = new AppDbContext())
 {
-    Console.WriteLine($"{rdr.GetInt32(0)}: {rdr.GetString(1)} - {rdr.GetInt32(2)} years old");
-}
+    db.Database.EnsureCreated(); // Создание базы данных и таблиц, если они не существуют
 
-con.Close();
+    // Добавление пользователя
+    db.Users.Add(new User { Name = "Alx", Age = 26 });
+    db.SaveChanges();
+
+    // Запрос пользователей
+    foreach (var user in db.Users)
+    {
+        Console.WriteLine($"{user.Id}: {user.Name} - {user.Age} years old");
+    }
+}
 
 Console.ReadLine();
