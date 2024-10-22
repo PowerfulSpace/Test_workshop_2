59904d64e29d5013e0ff1b48a5c5c72d4c218cb3
Alexander <sany199574@gmail.com>
2024-10-22 16:17:11 +0300

    Добавлен тестовый проект по MongoDB

---
+++ src/Test_workshop_2/TestApp_Easy_MongoDb/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace TestApp_Easy_MongoDb.Models
+{
+    // Класс для представления документа в базе данных
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+    }
+}
---
+++ src/Test_workshop_2/TestApp_Easy_MongoDb/Program.cs
@@ -0,0 +1,28 @@
+﻿// Подключение к серверу MongoDB (локально)
+using MongoDB.Bson;
+using MongoDB.Driver;
+using TestApp_Easy_MongoDb.Models;
+
+var client = new MongoClient("mongodb://localhost:27017");
+
+// Выбираем базу данных (создается автоматически, если её нет)
+var database = client.GetDatabase("testdb");
+
+// Выбираем коллекцию (таблицу в реляционных базах) пользователей
+var collection = database.GetCollection<User>("users");
+
+// Создаем нового пользователя
+var newUser = new User { Name = "Alice", Age = 25 };
+
+// Вставляем документ в коллекцию
+await collection.InsertOneAsync(newUser);
+Console.WriteLine("User inserted");
+
+// Чтение данных из коллекции
+var users = await collection.Find(new BsonDocument()).ToListAsync();
+foreach (var user in users)
+{
+    Console.WriteLine($"ID: {user.Id}, Name: {user.Name}, Age: {user.Age}");
+}
+
+Console.WriteLine("Done");
