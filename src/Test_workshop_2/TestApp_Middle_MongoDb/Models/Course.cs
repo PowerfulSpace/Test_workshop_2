9fc7bc1f294594dce18a27afab2a3fb1116dae51
Alexander <sany199574@gmail.com>
2024-10-22 17:39:18 +0300

    убран вспомгательный комментарий

--- src/Test_workshop_2/TestApp_Middle_MongoDb/Models/Course.cs
+++ src/Test_workshop_2/TestApp_Middle_MongoDb/Models/Course.cs
@@ -6,7 +6,7 @@ namespace TestApp_Middle_MongoDb.Models
     public class Course
     {
         [BsonId]
-        public ObjectId Id { get; set; } // Используем ObjectId вместо Guid
+        public ObjectId Id { get; set; }
         public string Name { get; set; } = null!;
         public List<ObjectId> StudentIds { get; set; } = new List<ObjectId>(); // Список идентификаторов студентов
     }
