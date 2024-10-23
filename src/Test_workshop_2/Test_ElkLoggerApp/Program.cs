9d95cb4a2bd78e7a7ee7de9679c14676e2218b3f
Alexander <sany199574@gmail.com>
2024-10-24 00:25:14 +0300

    Добавлен проект, по логированию

---
+++ src/Test_workshop_2/Test_ElkLoggerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// Настройка логирования через Serilog с отправкой данных в Elasticsearch
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+// Настройка Serilog для отправки данных в Elasticsearch
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()  // Логи выводятся в консоль
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        AutoRegisterTemplate = true,             // Авто-регистрация шаблона индекса
+        IndexFormat = "applogs-{0:yyyy.MM.dd}",  // Формат индекса для логов
+        NumberOfShards = 1,                      // Количество шардов
+        NumberOfReplicas = 0                     // Количество реплик
+    })
+    .CreateLogger();
+
+// Пример различных логов
+Log.Information("Приложение запущено.");
+Log.Warning("Это тестовое предупреждение.");
+Log.Error("Это тестовая ошибка.");
+
+// Завершение работы логирования
+Log.CloseAndFlush();
+
+Console.ReadLine();
