3f489baae29f1a4b7a59def21861596bc21e200a
Alexander <sany199574@gmail.com>
2024-10-28 00:19:44 +0300

    Удалён вспомогательный алгоритм

--- src/Test_workshop_2/Test_App_sorting/Program.cs
+++ src/Test_workshop_2/Test_App_sorting/Program.cs
@@ -5,9 +5,5 @@ var orderedNumbers = numbers.OrderBy(n => n);
 foreach (int i in orderedNumbers)
     Console.WriteLine(i);
 
-string[] people = { "Tom", "Bob", "Sam" };
-var orderedPeople = people.OrderBy(p => p);
-foreach (var p in orderedPeople)
-    Console.WriteLine(p);
 
 Console.ReadLine();
