c781c6c1743a28b8750637b9a4ca2dfbdc23f023
Alexander <sany199574@gmail.com>
2024-10-28 22:12:17 +0300

    Переписан алгоритм

--- src/Test_workshop_2/Test_App_sorting/Program.cs
+++ src/Test_workshop_2/Test_App_sorting/Program.cs
@@ -1,17 +1,12 @@
-﻿string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+﻿
+string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
 
-var selectedPeople = new List<string>();
-
-foreach (string person in people)
-{
-
-    if (person.ToUpper().StartsWith("T"))
-        selectedPeople.Add(person);
-}
-
-selectedPeople.Sort();
+// создаем новый список для результатов
+var selectedPeople = from p in people // передаем каждый элемент из people в переменную p
+                     where p.ToUpper().StartsWith("T") //фильтрация по критерию
+                     orderby p  // упорядочиваем по возрастанию
+                     select p; // выбираем объект в создаваемую коллекцию
 
 foreach (string person in selectedPeople)
     Console.WriteLine(person);
-
 Console.ReadLine();
