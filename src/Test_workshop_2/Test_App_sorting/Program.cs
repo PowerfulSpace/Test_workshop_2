e3f4bbd14fcfc53bcc2a8fe21d3266aa0a9f1ff4
Alexander <sany199574@gmail.com>
2024-10-30 00:00:37 +0300

    Убран вспомогательный комментарий

--- src/Test_workshop_2/Test_App_sorting/Program.cs
+++ src/Test_workshop_2/Test_App_sorting/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill"};
 
-// создаем новый список для результатов
+
 var selectedPeople = from p in people // передаем каждый элемент из people в переменную p
                      where p.ToUpper().StartsWith("T") //фильтрация по критерию
                      orderby p  // упорядочиваем по возрастанию
