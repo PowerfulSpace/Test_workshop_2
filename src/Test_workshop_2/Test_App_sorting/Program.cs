da07c33c5d86df8821c5a67507b67a0fdc19819f
Alexander <sany199574@gmail.com>
2024-11-01 23:35:59 +0300

    Удалён метод  - Withdraw

--- src/Test_workshop_2/Test_App_sorting/Program.cs
+++ src/Test_workshop_2/Test_App_sorting/Program.cs
@@ -11,17 +11,6 @@
         Balance += amount;
     }
 
-    public virtual void Withdraw(decimal amount)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Сумма должна быть положительной.");
-        }
-        if (amount > Balance)
-        {
-            throw new InvalidOperationException("Недостаточно средств.");
-        }
-        Balance -= amount;
-    }
+
 }
 
