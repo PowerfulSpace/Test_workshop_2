4622a404a16efca2fbc37b89287e2a74773840f5
Alexander <sany199574@gmail.com>
2024-11-01 23:36:10 +0300

    Удаёлн метод   -Deposit

--- src/Test_workshop_2/Test_App_sorting/Program.cs
+++ src/Test_workshop_2/Test_App_sorting/Program.cs
@@ -2,14 +2,6 @@
 {
     public decimal Balance { get; protected set; }
 
-    public virtual void Deposit(decimal amount)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Сумма должна быть положительной.");
-        }
-        Balance += amount;
-    }
 
 
 }
