3af8a7d58e2e1f786b288483815ab5b3279d8214
Alexander <sany199574@gmail.com>
2024-11-03 05:50:29 +0300

    Добавлено ещё дополнение к вызову кода

--- src/Test_workshop_2/FileWriter_Test_Pattern_Finalize/Program.cs
+++ src/Test_workshop_2/FileWriter_Test_Pattern_Finalize/Program.cs
@@ -7,7 +7,12 @@ using (var writer = new FileWriter("example.txt"))
     writer.WriteData("Hello, world!");
 } //
 
+// Также возможно использовать без using, но с вызовом Dispose вручную:
+var writer2 = new FileWriter("example2.txt");
+writer2.WriteData("Manual dispose call.");
+writer2.Dispose();
 
+// Если Dispose не вызван, Finalize сработает при сборке мусора
 
 
 Console.ReadLine();
