4ee4ccaee4d3218f6f781fdfb4f51e846189b467
Alexander <sany199574@gmail.com>
2024-11-09 17:14:00 +0300

    Добавлен пример с используемумым semaphore синхронно

---
+++ src/Test_workshop_2/Test_Synchronization_SemaphoreSlim/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+var account = new BankAccount(1000);
+
+Thread t1 = new Thread(() => {
+    for (int i = 0; i < 3; i++)
+        account.Deposit(200);
+})
+{ Name = "Thread 1" };
+
+Thread t2 = new Thread(() => {
+    for (int i = 0; i < 3; i++)
+        account.Withdraw(150);
+})
+{ Name = "Thread 2" };
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+class BankAccount
+{
+    private decimal balance;
+    private SemaphoreSlim semaphore = new SemaphoreSlim(1, 1); // Разрешаем только одному потоку выполнять операции
+
+    public BankAccount(decimal initialBalance)
+    {
+        balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        semaphore.Wait(); // Захватываем семафор синхронно
+        try
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} is depositing {amount}");
+            balance += amount;
+            Console.WriteLine($"New balance after deposit: {balance}");
+        }
+        finally
+        {
+            semaphore.Release(); // Обязательно освобождаем семафор
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        semaphore.Wait(); // Захватываем семафор синхронно
+        try
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} is withdrawing {amount}");
+            if (balance >= amount)
+            {
+                balance -= amount;
+                Console.WriteLine($"New balance after withdrawal: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds for withdrawal");
+            }
+        }
+        finally
+        {
+            semaphore.Release(); // Обязательно освобождаем семафор
+        }
+    }
+}
