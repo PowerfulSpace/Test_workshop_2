0c45aa3d253e65c3d8b51e26d4784383e7480cac
Alexander <sany199574@gmail.com>
2024-11-13 12:42:29 +0300

    Добавлен Пример BarrierExample

---
+++ src/Test_workshop_2/Test_BarrierExample/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+for (int i = 0; i < 4; i++)
+{
+    Thread t = new Thread(DoWork) { Name = $"Thread {i + 1}" };
+    t.Start();
+}
+
+Console.ReadLine();
+
+public class BarrierExample
+{
+    static Barrier barrier = new Barrier(4, (b) =>
+    {
+        // Эта часть кода выполняется после того, как все потоки достигли барьера
+        Console.WriteLine("Все потоки достигли барьера на фазе " + b.CurrentPhaseNumber);
+    });
+
+
+    private static void DoWork()
+    {
+        for (int i = 0; i < 3; i++) // 3 фазы работы
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} выполняет работу на фазе {i}");
+            Thread.Sleep(new Random().Next(1000, 3000)); // Симуляция работы
+
+            // Поток достигает барьера и ждет других
+            Console.WriteLine($"{Thread.CurrentThread.Name} достиг барьера на фазе {i}");
+            barrier.SignalAndWait();
+        }
+    }
+}
