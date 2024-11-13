5f272d238e4e81a42a0a318fcdc7e50ce8a64f3f
Alexander <sany199574@gmail.com>
2024-11-13 12:57:42 +0300

    Добавлен пример использующий синхронизацию CountdownEvent

---
+++ src/Test_workshop_2/Test_CountdownEvent/Program.cs
@@ -0,0 +1,22 @@
+﻿
+CountdownEvent countdown = new CountdownEvent(10);
+
+for (int i = 0; i < 100; i++)
+{
+    Task.Run(() => DoWork(i));
+}
+
+// Главный поток ожидает завершения 10 операций
+Console.WriteLine("Главный поток ждет завершения 10 операций.");
+countdown.Wait();
+Console.WriteLine("10 операций выполнены. Главный поток продолжает работу.");
+
+void DoWork(int id)
+{
+    Console.WriteLine($"Поток {id} начинает работу.");
+    Thread.Sleep(new Random().Next(500, 2000)); // Симуляция работы
+    Console.WriteLine($"Поток {id} завершил работу.");
+
+    // Сигнализируем о завершении работы
+    countdown.Signal();
+}
