e77cb98aa33d85f7178a8042211fe2b6e6ee92f6
Alexander <sany199574@gmail.com>
2024-11-13 13:53:35 +0300

    Добавлен пример Interlock

--- src/Test_workshop_2/Test_CountdownEvent/Program.cs
+++ src/Test_workshop_2/Test_CountdownEvent/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 CountdownEvent countdown = new CountdownEvent(10);
 
-for (int i = 0; i < 100; i++)
+for (int i = 0; i < 10; i++)
 {
     Task.Run(() => DoWork(i));
 }
---
+++ src/Test_workshop_2/Test_InterlockedExample/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+int counter = 0;
+
+Thread[] threads = new Thread[10];
+
+for (int i = 0; i < 5; i++)
+{
+    threads[i] = new Thread(Increment);
+    threads[i + 5] = new Thread(Decrement);
+}
+
+foreach (Thread t in threads)
+{
+    t.Start();
+}
+
+foreach (Thread t in threads)
+{
+    t.Join();
+}
+
+Console.WriteLine($"Итоговое значение счетчика: {counter}");
+
+Console.ReadLine();
+
+
+void Increment()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        Interlocked.Increment(ref counter);
+    }
+}
+
+void Decrement()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        Interlocked.Decrement(ref counter);
+    }
+}
