73f28305b69147305989d0b66b3386c9b6f33e88
Alexander <sany199574@gmail.com>
2024-11-16 00:18:17 +0300

    Исправлены ошибки

--- src/Test_workshop_2/Test_CountdownEvent/Program.cs
+++ src/Test_workshop_2/Test_CountdownEvent/Program.cs
@@ -6,8 +6,20 @@ for (int i = 0; i < 10; i++)
     Task.Run(() => DoWork(i));
 }
 
-
+// Главный поток ожидает завершения 10 операций
+Console.WriteLine("Главный поток ждет завершения 10 операций.");
+countdown.Wait();
+Console.WriteLine("10 операций выполнены. Главный поток продолжает работу.");
 
 
 Console.ReadLine();
 
+void DoWork(int id)
+{
+    Console.WriteLine($"Поток {id} начинает работу.");
+    Thread.Sleep(new Random().Next(500, 2000)); // Симуляция работы
+    Console.WriteLine($"Поток {id} завершил работу.");
+
+    // Сигнализируем о завершении работы
+    countdown.Signal();
+}
