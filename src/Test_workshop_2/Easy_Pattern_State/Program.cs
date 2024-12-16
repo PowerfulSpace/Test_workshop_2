872654c1e48f5621e93b230d5d0c14780bd1a6c6
Alexander <sany199574@gmail.com>
2024-12-16 04:04:41 +0300

    Добавлен пример паттерна состояния

---
+++ src/Test_workshop_2/Easy_Pattern_State/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+var context = new Context(new ConcreteStateA());
+
+Console.ReadLine();
+context.Request();
+
+Console.ReadLine();
+context.Request();
+
+Console.ReadLine();
+context.Request();
+
+
+Console.ReadLine();
+
+
+public interface IState
+{
+    void Handle(Context context);
+}
+public class Context
+{
+    private IState _state;
+
+    public Context(IState state)
+    {
+        TransitionTo(state);
+    }
+
+    public void TransitionTo(IState state)
+    {
+        Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+        _state = state;
+    }
+
+    public void Request()
+    {
+        _state.Handle(this);
+    }
+}
+public class ConcreteStateA : IState
+{
+    public void Handle(Context context)
+    {
+        Console.WriteLine("ConcreteStateA handles request.");
+        context.TransitionTo(new ConcreteStateB());
+    }
+}
+
+public class ConcreteStateB : IState
+{
+    public void Handle(Context context)
+    {
+        Console.WriteLine("ConcreteStateB handles request.");
+        context.TransitionTo(new ConcreteStateA());
+    }
+}
