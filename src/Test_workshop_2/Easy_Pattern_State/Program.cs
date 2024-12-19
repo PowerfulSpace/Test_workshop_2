3060f88e54af2a52f38c9efc416092d32c79abeb
Alexander <sany199574@gmail.com>
2024-12-19 23:21:42 +0300

    Убрнал класс  - Context

--- src/Test_workshop_2/Easy_Pattern_State/Program.cs
+++ src/Test_workshop_2/Easy_Pattern_State/Program.cs
@@ -19,23 +19,4 @@ public interface IState
 {
     void Handle(Context context);
 }
-public class Context
-{
-    private IState _state;
 
-    public Context(IState state)
-    {
-        TransitionTo(state);
-    }
-
-    public void TransitionTo(IState state)
-    {
-        Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
-        _state = state;
-    }
-
-    public void Request()
-    {
-        _state.Handle(this);
-    }
-}
