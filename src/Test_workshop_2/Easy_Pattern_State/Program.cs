d5336cc11565a50ed5c7afb161e67f32c7939032
Alexander <sany199574@gmail.com>
2024-12-19 23:21:24 +0300

    Убран класс  - ConcreteStateA

--- src/Test_workshop_2/Easy_Pattern_State/Program.cs
+++ src/Test_workshop_2/Easy_Pattern_State/Program.cs
@@ -39,12 +39,3 @@ public class Context
         _state.Handle(this);
     }
 }
-public class ConcreteStateA : IState
-{
-    public void Handle(Context context)
-    {
-        Console.WriteLine("ConcreteStateA handles request.");
-        context.TransitionTo(new ConcreteStateB());
-    }
-}
-
