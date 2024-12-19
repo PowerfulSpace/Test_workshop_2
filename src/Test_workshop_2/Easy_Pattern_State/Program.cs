cf228b876beb8ecc648f88061db29337b3bfd760
Alexander <sany199574@gmail.com>
2024-12-19 23:22:58 +0300

    Добавлены классы  - Context  - ConcreteStateA  - ConcreteStateB

--- src/Test_workshop_2/Easy_Pattern_State/Program.cs
+++ src/Test_workshop_2/Easy_Pattern_State/Program.cs
@@ -19,4 +19,40 @@ public interface IState
 {
     void Handle(Context context);
 }
+public class Context
+{
+    private IState _state;
 
+    public Context(IState state)
+    {
+        TransitionTo(state);
+    }
+
+    public void TransitionTo(IState state)
+    {
+        Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
+        _state = state;
+    }
+
+    public void Request()
+    {
+        _state.Handle(this);
+    }
+}
+public class ConcreteStateA : IState
+{
+    public void Handle(Context context)
+    {
+        Console.WriteLine("ConcreteStateA handles request.");
+        context.TransitionTo(new ConcreteStateB());
+    }
+}
+
+public class ConcreteStateB : IState
+{
+    public void Handle(Context context)
+    {
+        Console.WriteLine("ConcreteStateB handles request.");
+        context.TransitionTo(new ConcreteStateA());
+    }
+}
