00947d486759f6b88dce1d3b658f6305c020c4cf
Alexander <sany199574@gmail.com>
2024-12-23 09:26:59 +0300

    Добавлено приложение занимается поиском максимальных чисел из масиива при произведении

---
+++ src/Test_workshop_2/MaxProduct/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int[] array = { 10, 20, -10, -20, 5, 8, 3, -15 };
+
+Console.WriteLine(FindMaxProduct(array));
+
+Console.ReadLine();
+
+
+int FindMaxProduct(int[] array)
+{
+    if (array.Length < 2)
+    {
+        throw new Exception("Массив должен содержать хотя бы два элемента.");
+    }
+
+    List<int> nums = array.ToList();
+    nums.Sort();
+
+    int product1 = nums[nums.Count - 1] * nums[nums.Count - 2];
+    int product2 = nums[0] * nums[1];
+
+    return Math.Max(product1, product2);
+}
--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,19 +1,10 @@
 ﻿
 
-int[] array = { 10, 20, -10, -20, 5, 8, 3, -15 };
-
-Console.WriteLine(FindMaxProduct(array));
 
 Console.ReadLine();
 
 
-int FindMaxProduct(int[] array)
+void Method(int[] array)
 {
-    if (array.Length < 2) { throw new Exception("Массив должен содержать хотя бы два элемента."); }
-    if (array.Length == 2) { return array[0] * array[1]; }
 
-    List<int> nums = array.ToList();
-    nums.Sort();
-
-    return nums[nums.Count - 1] * nums[nums.Count - 2];
 }
