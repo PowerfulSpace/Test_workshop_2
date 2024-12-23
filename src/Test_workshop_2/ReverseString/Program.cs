6ab0d608c370a381e4edf99b308324377a2159f2
Alexander <sany199574@gmail.com>
2024-12-23 10:14:36 +0300

    Добавлен проект GetReverseString

---
+++ src/Test_workshop_2/ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Text;
+
+string str = "hello";
+
+
+Console.WriteLine(GetReverseString(str));
+
+
+
+Console.ReadLine();
+
+
+
+string GetReverseString(string str)
+{
+    char[] chars = str.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
+}
--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,10 +1,62 @@
 ﻿
 
+using System.Text;
+
+string str = "hello";
+
+
+Console.WriteLine(GetReverseString1(str));
+Console.WriteLine(GetReverseString2(str));
+Console.WriteLine(GetReverseString3(str));
+Console.WriteLine(GetReverseString4(str));
+
 
 Console.ReadLine();
 
 
-void Method(int[] array)
+string GetReverseString1(string str)
 {
+    Stack<char> chars = new Stack<char>();
+    StringBuilder sb = new StringBuilder();
 
+    foreach (char c in str)
+    {
+        chars.Push(c);
+    }
+    foreach (char c in chars)
+    {
+        sb.Append(c);
+    }
+
+    return sb.ToString();
+}
+
+string GetReverseString2(string str)
+{
+    var s = str.Reverse();
+    var result = string.Concat(s);
+
+    return result;
+}
+
+string GetReverseString3(string str)
+{
+    char[] chars = str.ToCharArray();
+    char temp;
+
+    for (int i = 0,j = str.Length - 1; i < str.Length / 2; i++,j--)
+    {
+        temp = str[i];
+        chars[i] = str[j];
+        chars[j] = temp;
+    }
+
+    return new string(chars);
+}
+
+string GetReverseString4(string str)
+{
+    char[] chars = str.ToCharArray();
+    Array.Reverse(chars);
+    return new string(chars);
 }
