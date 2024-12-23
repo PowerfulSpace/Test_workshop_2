4b61ecb3ad9580599827548a99d920f1866f1ca6
Alexander <sany199574@gmail.com>
2024-12-23 06:53:37 +0300

    Добавлен алгоритм

---
+++ src/Test_workshop_2/SimpleNum/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+
+
+List<int> result = FindSimpleNum(array);
+
+
+foreach (int i in result)
+{
+    Console.Write(i + " ");
+}
+
+
+Console.ReadLine();
+
+
+
+
+List<int> FindSimpleNum(int[] array)
+{
+    List<int> result = new List<int>();
+
+    foreach (int number in array)
+    {
+        if (number <= 1) continue;
+
+        bool isSimple = true;
+
+        int limit = (int)Math.Sqrt(number);
+
+        for (int divisor = 2; divisor <= limit; divisor++)
+        {
+            if (number % divisor == 0)
+            {
+                isSimple = false;
+                break;
+            }
+        }
+
+        if (isSimple)
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+}
