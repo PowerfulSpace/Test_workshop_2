5cca3ada9af61a0c3b5b8d6fa1cc330f60c1e0ee
Alexander <sany199574@gmail.com>
2024-12-25 15:19:54 +0300

    Добавлен алгоритм  - FindSumtwoNumber

---
+++ src/Test_workshop_2/FindSumtwoNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] array = { 10, 6, 35, 8, 16, 17, 14, 48, 29, 40, 21, 42, 47, 4, 38 };
+
+int s = 50;
+
+var result = FindSumtwoNumber(array, s);
+
+Console.WriteLine(result.Item1 + " " + result.Item2);
+
+Console.ReadLine();
+
+
+(int, int) FindSumtwoNumber(int[] array, int s)
+{
+    HashSet<int> seen = new HashSet<int>();
+
+    foreach (int num in array)
+    {
+        int complement = s - num;
+        if (seen.Contains(complement))
+        {
+            return (complement, num);
+        }
+        seen.Add(num);
+    }
+
+    throw new Exception($"Нет пар, которые в сумме образуют {s}");
+}
--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,36 +1,4 @@
 ﻿
-int[] array = { 10, 6, 35, 8, 16, 17, 14, 48, 29, 40, 21, 42, 47, 4, 38 };
 
-int s = 50;
-
-var result = FindSumtwoNumber(array, s);
-
-Console.WriteLine(result.Item1 + " " + result.Item2);
 
 Console.ReadLine();
-
-
-(int,int) FindSumtwoNumber(int[] array, int s)
-{
-    int first = 0;
-    int second = 0;
-    int remainder = 0;
-
-    for (int i = 0; i < array.Length; i++)
-	{
-        first = array[i];
-        
-        remainder = s - first;
-
-		for (int j = i + 1; j < array.Length; j++)
-		{
-            if(remainder == array[j])
-            {
-                second = array[j];
-                return (first, second);
-            }
-        }
-    }
-
-    throw new Exception($"Нет пар которые в сумме образовывают {s}");
-}
