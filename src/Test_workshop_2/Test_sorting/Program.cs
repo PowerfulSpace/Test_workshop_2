33592dfd73b400fbcf22a166871ca3140934aaab
Alexander <sany199574@gmail.com>
2024-12-25 10:41:55 +0300

    реализована сортировка

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,14 +1,49 @@
 ﻿
+
 int[] numbers = { 20, 23, 53, 69, 70, 10, 22, 69, 17, 69 };
 
-Sort(numbers);
+QuickSort(numbers, 0, numbers.Length - 1);
+
+Console.WriteLine(string.Join(", ", numbers));
 
 Console.ReadLine();
 
 
 
 
-void Sort(int[] array)
+void QuickSort(int[] array, int left, int right)
 {
+    if(left >= right) { return; }
 
+    int pivot = Partition(array, left, right);
+
+
+    Partition(array, left, pivot - 1);
+    Partition(array, pivot + 1, right);
+}
+
+int Partition(int[] array, int left, int right)
+{
+    int pivot = array[right];
+    int i = left - 1;
+
+    for (int j = left; j < right; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            Swap(array, i, j);
+        }
+    }
+
+    Swap(array, i + 1, right);
+
+    return i + 1;
+}
+
+void Swap(int[] array, int i, int j)
+{
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
 }
