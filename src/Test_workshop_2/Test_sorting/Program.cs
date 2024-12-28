0e2a0d16833fc4e7016363e8a12e47c6c5e08f00
Alexander <sany199574@gmail.com>
2024-12-29 00:01:19 +0300

    Добавлены данные

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,7 +1,26 @@
 ﻿
 
+int[,] maze = new int[,]
+{
+    { -1, 1, 0, 1, 1 },
+    { 0, 1, 0, 1, 2 },
+    { 1, 1, 0, 1, 1 },
+    { 0, 1, 1, 1, 0 },
+    { 1, 0, 0, 1, 1 }
+};
+
+
+Console.WriteLine(GetMinPath(maze));
 
 Console.ReadLine();
 
+
+int GetMinPath(int[,] maze)
+{
+
+
+
+    return 4324;
+}
 
 
