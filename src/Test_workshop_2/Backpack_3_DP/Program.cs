be71665ae6c2c0b599e94df25f8ba4f6a094f1a4
Alexander <sany199574@gmail.com>
2025-01-03 03:09:47 +0300

    Добавлен вариант алгоритма, через динамическое.

---
+++ src/Test_workshop_2/Backpack_3_DP/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int maxWeight = 15; // Максимальный вес рюкзака
+int[] weights = { 1, 3, 4, 5, 6, 7 }; // Веса предметов
+int[] values = { 1, 4, 5, 7, 10, 13 }; // Стоимости предметов
+
+Console.WriteLine(FindMaximumValueDP(weights, values, maxWeight));
+
+Console.ReadLine();
+
+
+
+int FindMaximumValueDP(int[] weights, int[] values, int maxWeight)
+{
+    int n = weights.Length;
+    int[,] dp = new int[n + 1, maxWeight + 1];
+
+    // Заполняем таблицу dp
+    for (int i = 1; i <= n; i++)
+    {
+        for (int w = 1; w <= maxWeight; w++)
+        {
+            if (weights[i - 1] <= w) // Если текущий предмет помещается в рюкзак
+            {
+                dp[i, w] = Math.Max(
+                    dp[i - 1, w], // Не брать текущий предмет
+                    values[i - 1] + dp[i - 1, w - weights[i - 1]] // Взять текущий предмет
+                );
+            }
+            else
+            {
+                dp[i, w] = dp[i - 1, w]; // Если предмет не помещается
+            }
+        }
+    }
+
+    // Максимальная стоимость будет в dp[n, maxWeight]
+    return dp[n, maxWeight];
+}
