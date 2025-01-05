9ef5769e2191a22e25858504cafd8347a7305f18
Alexander <sany199574@gmail.com>
2025-01-06 02:02:31 +0300

    Добавлен алгоритм FindMostFrequentNumber

---
+++ src/Test_workshop_2/FindMostFrequentNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] numbers = { 4, 5, 6, 7, 4, 3, 2, 4, 5, 5, 8, 9, 4 };
+
+Console.WriteLine(FindMostFrequentNumber(numbers));
+
+Console.ReadLine();
+
+
+int FindMostFrequentNumber(int[] numbers)
+{
+    Dictionary<int, int> nums = new Dictionary<int, int>();
+
+    foreach (int number in numbers)
+    {
+        if (nums.ContainsKey(number))
+        {
+            nums[number]++;
+        }
+        else
+        {
+            nums.Add(number, 1);
+        }
+    }
+
+    int result = nums.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+
+    return result;
+}
--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,8 +1,6 @@
 ﻿
-int[] numbers = { 4, 5, 6, 7, 4, 3, 2, 4, 5, 5, 8, 9, 4 };
-
-Console.WriteLine(FindMaxNumber(numbers));
 
 Console.ReadLine();
+
 
 
