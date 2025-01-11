df921285a645c26faf16d9e903f16a7b17b0e15f
Alexander <sany199574@gmail.com>
2025-01-11 11:08:32 +0300

    Переписан код

--- src/Test_workshop_2/GetShortestPath_Rover/Program.cs
+++ src/Test_workshop_2/GetShortestPath_Rover/Program.cs
@@ -1,6 +1,4 @@
 ﻿
-
-using System.IO;
 using System.Text;
 
 int[,] space = new int[5, 5]
@@ -18,73 +16,37 @@ Console.WriteLine(result);
 Console.ReadLine();
 
 
+
 string GetShortestPath(int[,] space)
 {
-
     for (int i = 1; i < space.GetLength(0); i++)
     {
-        space[i, 0] = space[i, 0] + space[i - 1, 0];
+        space[i, 0] += space[i - 1, 0];
     }
     for (int i = 1; i < space.GetLength(1); i++)
     {
-        space[0, i] = space[0, i] + space[0, i - 1];
+        space[0, i] += space[0, i - 1];
     }
 
     for (int i = 1; i < space.GetLength(0); i++)
     {
         for (int j = 1; j < space.GetLength(1); j++)
         {
-            space[i, j] = Math.Min(
-                space[i - 1, j] + space[i, j],
-                space[i, j - 1] + space[i, j]);
+            space[i, j] += Math.Min(space[i - 1, j], space[i, j - 1]);
         }
     }
 
-    PrintArray(space);
-    Console.WriteLine();
-
-    return PrintPath(ReversePath(space));
+    var path = RestorePath(space);
+    return FormatPath(path);
 }
 
-void PrintArray(int[,] space)
+Stack<(int row, int col)> RestorePath(int[,] space)
 {
-    for (int i = 0; i < space.GetLength(0); i++)
-    {
-        for (int j = 0; j < space.GetLength(1); j++)
-        {
-            Console.Write(space[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-
-string PrintPath(Stack<(int row, int col)> stack)
-{
-    StringBuilder sb = new StringBuilder();
-
-
-    while (stack.Count > 1)
-    {
-        var item = stack.Pop();
-        sb.Append($"({item.row}, {item.col}) -> ");
-    }
-
-    var endItem = stack.Pop();
-    sb.Append($"({endItem.row}, {endItem.col})");
-
-    return sb.ToString();
-}
-
-
-Stack<(int row, int col)> ReversePath(int[,] space)
-{
-    Stack<(int row, int col)> stack = new Stack<(int row, int col)>();
-
+    Stack<(int row, int col)> path = new Stack<(int row, int col)>();
     int row = space.GetLength(0) - 1;
     int col = space.GetLength(1) - 1;
 
-    stack.Push((row, col));
+    path.Push((row, col));
     while (row > 0 || col > 0)
     {
         if (row == 0)
@@ -103,8 +65,24 @@ Stack<(int row, int col)> ReversePath(int[,] space)
         {
             col--;
         }
-        stack.Push((row, col));
+        path.Push((row, col));
     }
 
-    return stack;
+    return path;
+}
+
+string FormatPath(Stack<(int row, int col)> path)
+{
+    StringBuilder sb = new StringBuilder();
+
+    while (path.Count > 1)
+    {
+        var (r, c) = path.Pop();
+        sb.Append($"({r}, {c}) -> ");
+    }
+
+    var last = path.Pop();
+    sb.Append($"({last.row}, {last.col})");
+
+    return sb.ToString();
 }
