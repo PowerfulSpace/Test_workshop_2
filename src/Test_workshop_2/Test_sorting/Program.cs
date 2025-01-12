9a9869cbac2016559da51afd223747007ae88f41
Alexander <sany199574@gmail.com>
2025-01-12 09:59:47 +0300

    Добавлен алгоритм выборки сотрудников

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,13 +1,42 @@
 ﻿
 
-int[,] space = new int[5, 5]
+//Написать LINQ-запрос для группировки списка сотрудников по их должности.
+
+// Список сотрудников
+var employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "Alice", Position = "Manager" },
+            new Employee { Id = 2, Name = "Bob", Position = "Developer" },
+            new Employee { Id = 3, Name = "Charlie", Position = "Developer" },
+            new Employee { Id = 4, Name = "Diana", Position = "Manager" },
+            new Employee { Id = 5, Name = "Eve", Position = "Designer" },
+        };
+
+
+
+var result = employees.GroupBy(x => x.Position);
+
+
+foreach (var group in result)
 {
-    { 1, 7, 7, 4, 3 },
-    { 9, 3, 4, 4, 4 },
-    { 6, 9, 7, 9, 3 },
-    { 3, 8, 9, 4, 4 },
-    { 6, 2, 3, 5, 1 }
-};
+    Console.WriteLine(group.Key);
+    foreach (var employee in group)
+    {
+        Console.WriteLine(employee.Name);
+    }
+    Console.WriteLine();
+}
+
+
 
 
 Console.ReadLine();
+
+
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+}
