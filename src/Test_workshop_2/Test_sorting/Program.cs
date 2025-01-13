d2a4efa2a58e846b8eb16f06f483d28e74be8e24
Alexander <sany199574@gmail.com>
2025-01-13 15:50:53 +0300

    TopRevenueProducts добавлена задача

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,12 +1,5 @@
 ﻿
-var words = new[] { "Apple", "banana", "APPLE", "Banana", "ORANGE", "orange", "grape" };
 
-var result = words.Select(x => x.ToLower()).Distinct();
-
-foreach (var word in result)
-{
-    Console.Write(word + " ");
-}
 
 Console.ReadLine();
 
---
+++ src/Test_workshop_2/TopRevenueProducts/Program.cs
@@ -0,0 +1,31 @@
+﻿
+var sales = new[]
+{
+    new { ProductId = 1, Quantity = 10, Price = 100 },
+    new { ProductId = 2, Quantity = 5, Price = 200 },
+    new { ProductId = 1, Quantity = 7, Price = 100 },
+    new { ProductId = 3, Quantity = 3, Price = 300 },
+    new { ProductId = 2, Quantity = 2, Price = 200 },
+    new { ProductId = 3, Quantity = 8, Price = 300 }
+};
+
+var result = sales
+    .GroupBy(x => x.ProductId)
+    .Select(x => new
+    {
+        ProductId = x.Key,
+        TotalRevenue = x.Sum(s => s.Quantity * s.Price)
+    })
+    .OrderByDescending(x => x.TotalRevenue)
+    .Take(3);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+
+Console.ReadLine();
+
+
+
