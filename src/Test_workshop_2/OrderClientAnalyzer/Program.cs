7e52ff4e4b7c44ddb8e669ddcddcc5da09ebd185
Alexander <sany199574@gmail.com>
2025-01-14 11:45:56 +0300

    Добавлена задача сортировка объеденения клиентов по заказам

---
+++ src/Test_workshop_2/OrderClientAnalyzer/Program.cs
@@ -0,0 +1,34 @@
+﻿
+var orders = new[]
+{
+    new { OrderId = 1, ClientId = 1, Amount = 500 },
+    new { OrderId = 2, ClientId = 2, Amount = 1500 },
+    new { OrderId = 3, ClientId = 1, Amount = 2000 },
+    new { OrderId = 4, ClientId = 3, Amount = 800 }
+};
+
+var clients = new[]
+{
+    new { ClientId = 1, Name = "Company A" },
+    new { ClientId = 2, Name = "Company B" },
+    new { ClientId = 3, Name = "Company C" }
+};
+
+
+
+var result = orders
+    .Where(x => x.Amount > 1000)
+    .Join(clients, o => o.ClientId, c => c.ClientId, (o, c) => new
+    {
+        OrderId = o.OrderId,
+        Name = c.Name,
+        Amount = o.Amount
+    })
+    .OrderByDescending(x => x.Amount);
+
+foreach (var item in result)
+{
+    Console.WriteLine($"Client: {item.Name}, Orders: [OrderId: {item.OrderId}, Amount: {item.Amount}]");
+}
+Console.ReadLine();
+
