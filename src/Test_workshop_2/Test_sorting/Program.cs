4788453012360f93452029fcccdb8480a7b93caa
Alexander <sany199574@gmail.com>
2025-01-14 15:21:53 +0300

    Добавлен алгоритм

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,4 +1,29 @@
 ﻿
+var employeesSet1 = new[]
+{
+    new { Id = 1, Name = "Alice" },
+    new { Id = 2, Name = "Bob" },
+    new { Id = 3, Name = "Charlie" }
+};
+
+var employeesSet2 = new[]
+{
+    new { Id = 2, Name = "Bob" },
+    new { Id = 3, Name = "Charlie" },
+    new { Id = 4, Name = "David" }
+};
+
+var result = employeesSet1.Intersect(employeesSet2);
+
+var result2 = employeesSet1.Intersect(employeesSet2);
+
+
+
+foreach (var employee in result)
+{
+    Console.WriteLine(employee);
+}
+
 
 
 Console.ReadLine();
