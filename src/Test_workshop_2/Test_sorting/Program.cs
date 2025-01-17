7c0936c6a94a4d3dd0fd63dfdbda9ddeb0e2ea9e
Alexander <sany199574@gmail.com>
2025-01-17 06:43:05 +0300

    Добавлена вторая реализация для алгоритма

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,6 +1,21 @@
 ﻿
 
+var numbers = new[] { 1, 2, 3, 4, 5, 6 };
+int targetSum = 7;
 
+var result = numbers
+    .SelectMany((x, i) => numbers.Skip(i + 1), (x, y) => new { x, y })
+    .Where(pair => pair.x + pair.y == targetSum)
+    .Select(pair => (pair.x, pair.y))
+    .ToList();
+
+//var pairs = numbers
+//         .SelectMany((x, i) => numbers
+//             .Skip(i + 1)
+//             .Where(y => x + y == targetSum)
+//             .Select(y => (x, y))
+//         )
+//         .ToList();
 
 Console.ReadLine();
 
