44739eb26a54238c2f57003792573038f74059de
Alexander <sany199574@gmail.com>
2025-01-17 18:30:06 +0300

    Добавлен проект  Нахождение суммы цифр в строковом представлении чисел

---
+++ src/Test_workshop_2/FindingTheSumOfDigitAStringRepresentationOfNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+var numberStrings = new[] { "123", "456", "789" };
+
+var result = numberStrings.Select(x => x.Sum(c => int.Parse(c.ToString())));
+
+
+foreach (var numberString in result)
+{
+    Console.Write(numberString + " ");
+}
+
+Console.ReadLine();
+
+
--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,14 +1,5 @@
 ﻿
 
-var numberStrings = new[] { "123", "456", "789" };
-
-var result = numberStrings.Select(x => x.Sum(c => int.Parse(c.ToString())));
-
-
-foreach (var numberString in result)
-{
-    Console.Write(numberString + " ");
-}
 
 Console.ReadLine();
 
