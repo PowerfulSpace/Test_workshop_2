60dc586b43a4ea1f595ce3e7ffb20630ce364cb3
Alexander <sany199574@gmail.com>
2025-01-17 17:47:32 +0300

    Добавлена задача

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,5 +1,14 @@
 ﻿
 
+var numberStrings = new[] { "123", "456", "789" };
+
+var result = numberStrings.Select(x => x.Sum(c => int.Parse(c.ToString())));
+
+
+foreach (var numberString in result)
+{
+    Console.Write(numberString + " ");
+}
 
 Console.ReadLine();
 
