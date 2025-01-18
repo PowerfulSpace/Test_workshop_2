47d7fe2f5cd02e5ed13bca23bc72a092b9ab4e7f
Alexander <sany199574@gmail.com>
2025-01-18 11:38:22 +0300

    Добавлена задача    -  Нахождение сотрудников с зарплатой выше среднего по отделу

---
+++ src/Test_workshop_2/FindingEmployeesWithSalariesAboveTheDepartmentAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿var employees = new[]
+{
+    new { Name = "Иван", Department = "ИТ", Salary = 100000 },
+    new { Name = "Мария", Department = "ИТ", Salary = 90000 },
+    new { Name = "Петр", Department = "ИТ", Salary = 110000 },
+    new { Name = "Анна", Department = "Маркетинг", Salary = 95000 },
+    new { Name = "Ольга", Department = "Маркетинг", Salary = 90000 }
+};
+
+var result = employees
+    .GroupBy(x => x.Department)
+    .SelectMany(g =>
+    {
+        var averageSalary = g.Average(emp => emp.Salary);
+        return g.Where(emp => emp.Salary > averageSalary);
+    });
+
+Console.WriteLine("Сотрудники с зарплатой выше средней по отделу:");
+foreach (var employee in result)
+{
+    Console.WriteLine($"{employee.Name} ({employee.Department})");
+}
+
+Console.ReadLine();
+
