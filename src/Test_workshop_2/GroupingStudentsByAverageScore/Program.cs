6b108709600d3cdc5ed3d5b01dfe0021a1b35006
Alexander <sany199574@gmail.com>
2025-01-18 10:40:33 +0300

    Добавлена задача  -  Группировка студентов по среднему баллу и определение количества студентов в каждой группе

---
+++ src/Test_workshop_2/GroupingStudentsByAverageScore/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+var students = new[]
+{
+    new { Name = "Алексей", Grades = new[] { 90, 85, 88 } },
+    new { Name = "Мария", Grades = new[] { 75, 80, 78 } },
+    new { Name = "Иван", Grades = new[] { 60, 65, 70 } },
+    new { Name = "Ольга", Grades = new[] { 95, 92, 98 } }
+};
+
+
+var result = students
+    .GroupBy(x => (int)x.Grades.Average() / 10)
+    .Select(x => new
+    {
+        Range = x.Key,
+        Count = x.Count()
+    });
+
+foreach (var group in result)
+{
+    Console.WriteLine($"Диапазон {group.Range * 10}-{group.Range * 10 + 9}: {group.Count} студента(ов)");
+}
+
+Console.ReadLine();
+
+
