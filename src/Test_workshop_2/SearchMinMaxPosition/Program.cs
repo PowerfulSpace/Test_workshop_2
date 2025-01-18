03e1a3c2862665b90f494a40493f1c048088fd8b
Alexander <sany199574@gmail.com>
2025-01-18 16:07:00 +0300

    Добавлена задача  - Поиск должностей с наибольшей и наименьшей средней зарплатой

---
+++ src/Test_workshop_2/SearchMinMaxPosition/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+var employees = new[]
+{
+    new { Name = "Иван", Position = "Разработчик", Salary = 100000 },
+    new { Name = "Мария", Position = "Аналитик", Salary = 90000 },
+    new { Name = "Петр", Position = "Разработчик", Salary = 110000 },
+    new { Name = "Анна", Position = "Менеджер", Salary = 95000 },
+    new { Name = "Ольга", Position = "Аналитик", Salary = 85000 }
+};
+
+var result = employees
+    .GroupBy(x => x.Position)
+    .Select(x => new
+    {
+        Position = x.Key,
+        SalaryAverage = x.Average(x => x.Salary)
+    });
+
+var min = result.OrderBy(x => x.SalaryAverage).FirstOrDefault();
+var max = result.OrderByDescending(x => x.SalaryAverage).FirstOrDefault();
+
+
+Console.WriteLine($"Должность с наивысшей средней зарплатой: {max.Position}");
+Console.WriteLine($"Должность с наименьшей средней зарплатой: {min.Position}");
+
+
+
+Console.ReadLine();
+
+
