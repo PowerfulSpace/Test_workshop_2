f97721a1436f5a7733b2150436d5a9b3fac34a03
Alexander <sany199574@gmail.com>
2025-01-18 07:45:04 +0300

    Добавлен алгоритм

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,5 +1,27 @@
 ﻿
 
+var students = new[]
+{
+    new { Name = "Алексей", Grades = new[] { 90, 85, 88 } },
+    new { Name = "Мария", Grades = new[] { 75, 80, 78 } },
+    new { Name = "Иван", Grades = new[] { 60, 65, 70 } },
+    new { Name = "Ольга", Grades = new[] { 95, 92, 98 } }
+};
+
+
+var result = students
+    .GroupBy(x => x.Grades.Average() % 10)
+    .Select(x => new
+    {
+        Range = x.Key,
+        Count = x.Count()
+    });
+
+foreach (var student in result)
+{
+    Console.WriteLine(student);
+}
+
 Console.ReadLine();
 
 
