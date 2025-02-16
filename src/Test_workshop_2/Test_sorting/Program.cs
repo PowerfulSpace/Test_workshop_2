d51c45728ee669605a4dd84da03c0756cc0ef5bf
Alexander <sany199574@gmail.com>
2025-02-17 00:34:10 +0300

    Добавлена простая сортировка

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,15 +1,10 @@
 ﻿
 
 
-
-using System.Text.RegularExpressions;
-
-string pattern = @"\b(\w+?)\s\1\b";
-string input = "This this is a nice day. What about this? This tastes good. I saw a a dog.";
-foreach (Match match in Regex.Matches(input, pattern, RegexOptions.IgnoreCase))
-    Console.WriteLine("{0} (duplicates '{1}') at position {2}",
-                      match.Value, match.Groups[1].Value, match.Index);
-
+int[] numbers = new int[] { 97, 45, 32, 65, 83, 23, 15 };
+Array.Sort(numbers);
+foreach (int n in numbers)
+    Console.WriteLine(n);
 
 Console.ReadLine();
 
