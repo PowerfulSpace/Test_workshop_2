cb993d429e0cff2bd09e21909be6725010eba32a
Alexander <sany199574@gmail.com>
2025-02-17 19:25:46 +0300

    Использование регулярного выражения для поиска повторений

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -1,11 +1,20 @@
 ﻿
 
 
-int[] numbers = new int[] { 97, 45, 32, 65, 83, 23, 15 };
-Array.Sort(numbers);
-foreach (int n in numbers)
-    Console.WriteLine(n);
+using System.Text.RegularExpressions;
 
+string s = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+Regex regex = new Regex(@"туп(\w*)");
+MatchCollection matches = regex.Matches(s);
+if (matches.Count > 0)
+{
+    foreach (Match match in matches)
+        Console.WriteLine(match.Value);
+}
+else
+{
+    Console.WriteLine("Совпадений не найдено");
+}
 Console.ReadLine();
 
 
