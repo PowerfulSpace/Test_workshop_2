42d365f4c621cc64c09b1872a5fb65f018458a2d
Alexander <sany199574@gmail.com>
2025-02-18 23:20:57 +0300

    Добавлена программа для парсинга сайта

---
+++ src/Test_workshop_2/Console_Parser_HtmlAgilityPack/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+// URL сайта, который будем парсить
+string url = "https://example.com";
+
+// Загружаем HTML-документ
+HtmlWeb web = new HtmlWeb();
+HtmlDocument doc = web.Load(url);
+
+// Извлекаем все ссылки (теги <a>)
+List<string> links = new List<string>();
+foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+{
+    string hrefValue = link.GetAttributeValue("href", string.Empty);
+    if (!string.IsNullOrEmpty(hrefValue))
+    {
+        links.Add(hrefValue);
+    }
+}
+
+// Выводим найденные ссылки
+Console.WriteLine("Найденные ссылки:");
+foreach (string link in links)
+{
+    Console.WriteLine(link);
+}
+
+
