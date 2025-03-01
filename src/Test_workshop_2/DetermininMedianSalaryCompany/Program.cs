cbb1656005185ae297d2fea46904366e9da93502
Alexander <sany199574@gmail.com>
2025-03-01 06:42:41 +0300

    Удалена лишняя проверка

--- src/Test_workshop_2/DetermininMedianSalaryCompany/Program.cs
+++ src/Test_workshop_2/DetermininMedianSalaryCompany/Program.cs
@@ -7,8 +7,6 @@ var dataService = new DataService();
 var data1 = await dataService.GetDataAsync(1);
 Console.WriteLine(data1);
 
-var data2 = await dataService.GetDataAsync(1);
-Console.WriteLine(data2);
 
 
 
