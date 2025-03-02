bbb50d26de5e3676e5680a1e36b32f894cf5405d
Alexander <sany199574@gmail.com>
2025-03-02 14:47:53 +0300

    реализован класс program

--- src/Test_workshop_2/PaymentProcessor/Program.cs
+++ src/Test_workshop_2/PaymentProcessor/Program.cs
@@ -1,5 +1,20 @@
 ﻿
+using PaymentProcessor.Interfaces;
+using PaymentProcessor.PaymentProcessors;
+using PaymentProcessor.Payments;
+using PaymentProcessor;
 
+IPaymentProcessor creditCardProcessor = new CreditCardPaymentProcessor();
+Payment creditCardPayment = new CreditCardPayment(creditCardProcessor);
+creditCardPayment.MakePayment(100.0m);
+
+IPaymentProcessor payPalProcessor = new PayPalPaymentProcessor();
+Payment payPalPayment = new PayPalPayment(payPalProcessor);
+payPalPayment.MakePayment(150.0m);
+
+IPaymentProcessor cryptoProcessor = new CryptocurrencyPaymentProcessor();
+Payment cryptoPayment = new CryptocurrencyPayment(cryptoProcessor);
+cryptoPayment.MakePayment(200.0m);
 
 
 Console.ReadLine();
