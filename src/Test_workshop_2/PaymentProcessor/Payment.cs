53bb9d117072fab2e26ae054b4aea13fa5918fa3
Alexander <sany199574@gmail.com>
2025-03-02 14:45:31 +0300

    Добавлен класс  - Payment

---
+++ src/Test_workshop_2/PaymentProcessor/Payment.cs
@@ -0,0 +1,19 @@
+﻿using PaymentProcessor.Interfaces;
+
+namespace PaymentProcessor
+{
+    public class Payment
+    {
+        protected IPaymentProcessor paymentProcessor;
+
+        public Payment(IPaymentProcessor paymentProcessor)
+        {
+            this.paymentProcessor = paymentProcessor;
+        }
+
+        public virtual void MakePayment(decimal amount)
+        {
+            paymentProcessor.ProcessPayment(amount);
+        }
+    }
+}
