fc4a76d044105aad3fa0cbd4cc59d9d62603b5d0
Alexander <sany199574@gmail.com>
2025-03-02 08:27:16 +0300

    Возвращён код на место

--- src/Test_workshop_2/DetermininMedianSalaryCompany/Program.cs
+++ src/Test_workshop_2/DetermininMedianSalaryCompany/Program.cs
@@ -7,6 +7,8 @@ var dataService = new DataService();
 var data1 = await dataService.GetDataAsync(1);
 Console.WriteLine(data1);
 
+var data2 = await dataService.GetDataAsync(1);
+Console.WriteLine(data2);
 
 
 
