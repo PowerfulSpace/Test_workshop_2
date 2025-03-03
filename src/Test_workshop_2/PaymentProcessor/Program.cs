ed7e66db99b29b1c4efc63d377e748814884aa28
Alexander <sany199574@gmail.com>
2025-03-03 18:12:39 +0300

    Изменил номерацию проектам

--- src/Test_workshop_2/PaymentProcessor/Program.cs
+++ src/Test_workshop_2/PaymentProcessor/Program.cs
@@ -8,7 +8,13 @@ IPaymentProcessor creditCardProcessor = new CreditCardPaymentProcessor();
 Payment creditCardPayment = new CreditCardPayment(creditCardProcessor);
 creditCardPayment.MakePayment(100.0m);
 
+IPaymentProcessor payPalProcessor = new PayPalPaymentProcessor();
+Payment payPalPayment = new PayPalPayment(payPalProcessor);
+payPalPayment.MakePayment(150.0m);
 
+IPaymentProcessor cryptoProcessor = new CryptocurrencyPaymentProcessor();
+Payment cryptoPayment = new CryptocurrencyPayment(cryptoProcessor);
+cryptoPayment.MakePayment(200.0m);
 
 
 Console.ReadLine();
