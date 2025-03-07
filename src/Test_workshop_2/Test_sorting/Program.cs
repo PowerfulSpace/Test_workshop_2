62ddd23d381e57b6e0c554986af8bc0178f70fe9
Alexander <sany199574@gmail.com>
2025-03-07 19:56:57 +0300

    Реализован код

--- src/Test_workshop_2/Test_sorting/Program.cs
+++ src/Test_workshop_2/Test_sorting/Program.cs
@@ -5,6 +5,25 @@ using System.Text.RegularExpressions;
 string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
                 @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
 
+var data = new string[]
+{
+    "tom@gmail.com",
+    "+12345678999",
+    "bob@yahoo.com",
+    "+13435465566",
+    "sam@yandex.ru",
+    "+43743989393"
+};
+
+Console.WriteLine("Email List");
+for (int i = 0; i < data.Length; i++)
+{
+    if (Regex.IsMatch(data[i], pattern, RegexOptions.IgnoreCase))
+    {
+        Console.WriteLine(data[i]);
+    }
+}
+
 Console.ReadLine();
 
 
